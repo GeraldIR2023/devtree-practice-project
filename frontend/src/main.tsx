9cae59fe170f84b384ec010bfa52f133387dc6c7
Gerald Corrales <geraldcorrales29@gmail.com>
2025-04-07 22:59:15 -0600

    Instalamos React Query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Router from "./router";
 import "./index.css";
 
+import React from "react";
+import Router from "./router";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Router />
+        <QueryClientProvider client={queryClient}>
+            <Router />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     </React.StrictMode>
 );
