03572c373ec04c956bb6adf36df161950a046bf3
Gerald Corrales <geraldcorrales29@gmail.com>
2025-04-16 12:14:02 -0600

    Agregamos el array de RS

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { social } from "../data/social";
+
 export default function LinkTreeView() {
+    const [devTreeLinks, setDevTreeLinks] = useState(social); //^ Lo colocamos en el state para mantenerlo sincronizado entre varias funciones
     return <div>LinkTreeView</div>;
 }
