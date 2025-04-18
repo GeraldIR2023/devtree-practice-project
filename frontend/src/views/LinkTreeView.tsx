719eff6d1f50f02bc1b05cdfb10a89f440727029
Gerald Corrales <geraldcorrales29@gmail.com>
2025-04-17 19:35:52 -0600

    Agregando el array de redes sociales

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
 
 export default function LinkTreeView() {
     const [devTreeLinks, setDevTreeLinks] = useState(social); //^ Lo colocamos en el state para mantenerlo sincronizado entre varias funciones
-    return <div>LinkTreeView</div>;
+    return (
+        <>
+            <div className=" space-y-5">
+                {devTreeLinks.map((item) => (
+                    <DevTreeInput />
+                ))}
+            </div>
+        </>
+    );
 }
