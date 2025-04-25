ef6d8b50d1c66520d355da4fd773740b60b85a64
Gerald Corrales <geraldcorrales29@gmail.com>
2025-04-25 16:43:36 -0600

    CVreando componentes de navegación reutilizables

---
+++ frontend/src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Logo() {
+    return (
+        <Link to="/">
+            <img
+                src="/logo.svg"
+                className="w-full block"
+                alt="Logotipo DevTree"
+            />
+        </Link>
+    );
+}
--- frontend/src/components/nav/HomeNavigation.tsx
+++ frontend/src/components/nav/HomeNavigation.tsx
@@ -5,8 +5,16 @@ export default function HomeNavigation() {
         <>
             <Link
                 to="/auth/login"
-                className=" text-white p-2 uppercase font-black text-xs cursor-pointer"
-            ></Link>
+                className="text-white p-2 uppercase font-black text-xs cursor-pointer"
+            >
+                Iniciar Sesión
+            </Link>
+            <Link
+                to="/auth/register"
+                className="bg-lime-500 text-slate-800 p-2 uppercase font-black text-xs cursor-pointer rounded-lg"
+            >
+                Registrarme
+            </Link>
         </>
     );
 }
