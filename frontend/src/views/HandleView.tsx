071f3fd0fbe360ab159ec13af1eb204f317f274e
Gerald Corrales <geraldcorrales29@gmail.com>
2025-04-24 23:13:26 -0600

    Consultando el handle con RQ

--- frontend/src/views/HandleView.tsx
+++ frontend/src/views/HandleView.tsx
@@ -1,3 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getUserByHandle } from "../api/DevTreeAPI";
+
 export default function HandleView() {
+    const params = useParams();
+    const handle = params.handle!;
+
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["handle", handle],
+        queryFn: () => getUserByHandle(handle),
+        retry: 1,
+    });
+
+    console.log(isLoading);
+    console.log(error);
+    console.log(data);
     return <div>HandleView</div>;
 }
