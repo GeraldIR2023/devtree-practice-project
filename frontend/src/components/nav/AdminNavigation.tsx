d2c217dcf1295da5cb95cd11696e8aba1bf004e6
Gerald Corrales <geraldcorrales29@gmail.com>
2025-04-26 16:22:19 -0600

    Cerramos sesión

--- frontend/src/components/nav/AdminNavigation.tsx
+++ frontend/src/components/nav/AdminNavigation.tsx
@@ -1,8 +1,17 @@
+import { useQueryClient } from "@tanstack/react-query";
+
 export default function AdminNavigation() {
+    const queryClient = useQueryClient();
+
+    const logout = () => {
+        localStorage.removeItem("AUTH_TOKEN"); //^ Limpiar el token del local storage
+        queryClient.invalidateQueries({ queryKey: ["user"] }); //^ Invalidar la cache de la query de usuario
+    };
+
     return (
         <button
             className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-            onClick={() => {}}
+            onClick={logout}
         >
             Cerrar Sesión
         </button>
